055aa73c38b6799623ee2b913ecf197dcf41c1fc
CesarChiodi <cesarchiodi@outlook.com>
2022-03-09 09:29:27 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace triangulos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int l1, l2, l3;
+            Console.WriteLine("informe a medida do primeiro lado do triangulo");
+            l1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("informe a medida do segundo lado do triangulo");
+            l2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("informe a medida do terceiro lado do triangulo");
+            l3 = int.Parse(Console.ReadLine());
+            if ((l1 == l2 || l2 == l3 || l1 == l3))
+                Console.WriteLine("isosceles");
+            else 
+            {
+                if ((l1 == l2) && (l2 == l3))
+                {
+                    Console.WriteLine("equilátero");
+                }
+                else
+                    Console.WriteLine("escaleno");
+            }
+        }
+    }
+}
